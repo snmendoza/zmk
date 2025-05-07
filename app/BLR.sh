a63187541688dea1799317549aa346af77c9dffe
snmendoza <sean.mendoza@me.com>
2025-05-07 13:53:39 -0700

    update build shell script (working)

--- app/BLR.sh
+++ app/BLR.sh
@@ -1,8 +1,30 @@
+#!/bin/bash
+set -e
 
-west build -p -d build/lefttp -b nice_nano_v2 -- -DSHIELD=sofle_left
-mv /Users/SeanMacbook/zephyr-sdk-0.15.0/zmkspiff/app/build/lefttp/zephyr/zmk.uf2 /Users/SeanMacbook/zephyr-sdk-0.15.0/zmkspiff/app/build/lefttp/zephyr/zmkl.uf2
-mv /Users/SeanMacbook/zephyr-sdk-0.15.0/zmkspiff/app/build/lefttp/zephyr/zmkl.uf2 /Users/SeanMacbook/zephyr-sdk-0.15.0/zmkspiff/app/build/
+# Clean and build right
+rm -rf build/right
+west build -d build/right -p -b nice_nano_v2 -- -DSHIELD=sofle_right
 
-west build -p -d build/righttp -b nice_nano_v2 -- -DSHIELD=sofle_right
-mv /Users/SeanMacbook/zephyr-sdk-0.15.0/zmkspiff/app/build/righttp/zephyr/zmk.uf2 /Users/SeanMacbook/zephyr-sdk-0.15.0/zmkspiff/app/build/righttp/zephyr/zmkr.uf2
-mv /Users/SeanMacbook/zephyr-sdk-0.15.0/zmkspiff/app/build/righttp/zephyr/zmkr.uf2 /Users/SeanMacbook/zephyr-sdk-0.15.0/zmkspiff/app/build/
+# Build lefttp
+rm -rf build/lefttp
+west build -d build/lefttp -p -b nice_nano_v2 -- -DSHIELD=sofle_left
+
+# Move lefttp .uf2 if it exists
+if [ -f build/lefttp/zephyr/zmk.uf2 ]; then
+    mv build/lefttp/zephyr/zmk.uf2 build/lefttp/zephyr/zmkl.uf2
+    mv build/lefttp/zephyr/zmkl.uf2 build/
+else
+    echo "Lefttp .uf2 not found!"
+fi
+
+# Clean and build righttp
+rm -rf build/righttp
+west build -d build/righttp -p -b nice_nano_v2 -- -DSHIELD=sofle_right
+
+# Move righttp .uf2 if it exists
+if [ -f build/righttp/zephyr/zmk.uf2 ]; then
+    mv build/righttp/zephyr/zmk.uf2 build/righttp/zephyr/zmkr.uf2
+    mv build/righttp/zephyr/zmkr.uf2 build/
+else
+    echo "Righttp .uf2 not found!"
+fi
